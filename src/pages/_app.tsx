88849c6a25e9495df11d04461e77fcb57ace47c7
Suji Park <psjhero94@naver.com>
2024-06-29 18:12:36 +0900

    공통 컴포넌트 구현

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
-import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { AlertContextProvider } from "@/contexts/AlertContext";
+import globalStyles from "@/styles/globalStyles";
+import { Global } from "@emotion/react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Global styles={globalStyles} />
+      <AlertContextProvider>
+        <Component {...pageProps} />;
+      </AlertContextProvider>
+    </>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,19 @@
 import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
+
+import Text from '@shared/Text'
+import Button from '@shared/Button'
+import Input from '@shared/Input'
+import TextField from '@shared/TextField'
+import Alert from '@shared/Alert'
+import { useAlertContext } from '@contexts/AlertContext'
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const { open } = useAlertContext()
+
   return (
     <>
       <Head>
@@ -14,100 +22,58 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={`${styles.main} ${inter.className}`}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>src/pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{" "}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
+      <main>
+      <div>
+      <Text typography="t1" display="block" color="red">
+        t1
+      </Text>
+      <Text typography="t2" color="blue">
+        t2
+      </Text>
+      <Text typography="t3">t3</Text>
+      <Text typography="t4">t4</Text>
+      <Text>t5</Text>
 
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-        </div>
+      <div style={{ height: 10, width: '100%', background: '#efefef' }} />
 
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
+      <Button>클릭해주세요</Button>
+      <Button color="success">클릭해주세요</Button>
+      <Button color="error">클릭해주세요</Button>
+      <Button color="success" weak={true}>
+        클릭해주세요
+      </Button>
+      <Button color="error" weak={true}>
+        클릭해주세요
+      </Button>
+      <Button full={true}>클릭해주세요</Button>
+      <Button full={true} disabled={true}>
+        클릭해주세요
+      </Button>
 
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
+      <div style={{ height: 10, width: '100%', background: '#efefef' }} />
 
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
+      <Input placeholder="로그인" aria-invalid={false} />
+      <Input aria-invalid={true} />
 
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
+      <TextField label="아이디" />
+      <TextField label="패스워드" hasError={true} />
+
+      <div style={{ height: 10, width: '100%', background: '#efefef' }} />
+
+      <Button
+        onClick={() => {
+          open({
+            title: '카드신청완료',
+            description: '내역페이지에서 확인해주세요',
+            onButtonClick: () => {
+              //
+            },
+          })
+        }}
+      >
+        Alert오픈
+      </Button>
+      </div>
       </main>
     </>
   );
