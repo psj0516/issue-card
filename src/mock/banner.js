13781f0998c89da13f6a85c565e83c5a4b2c73ec
Suji Park <psjhero94@naver.com>
2024-07-03 22:01:29 +0900

    배너 데이터 추가

--- src/mock/banner.js
+++ src/mock/banner.js
@@ -2,19 +2,19 @@ export const adBanners = [
   {
     title: "Easy all 카드",
     description: "기간이 얼마 남지 않았어요",
-    // link: "/card/0FCQ0P3fHwIuHfEkFb98",
+    link: "/card/ufP87CZOlv39iZ86R9rp",
     iconUrl: "https://cdn1.iconfinder.com/data/icons/filled-line-christmas-icons/75/_card_christmas-512.png",
   },
   {
     title: "굿데이올림카드",
     description: "신규회원 연회비 지원해드려요",
-    // link: "/card/1A1vyiHfHyZagksPfvsS",
+    link: "/card/5Ks2OtWt2f6I6o9yO6Np",
     iconUrl: "https://cdn4.iconfinder.com/data/icons/business-1219/24/Bank_Card-512.png",
   },
   {
-    title: "굿데이올림카드",
-    description: "신규회원 연회비 지원해드려요",
-    // link: "/card/1A1vyiHfHyZagksPfvsS",
+    title: "톡톡M 카드",
+    description: "아메리카노 쿠폰 제공해드려요",
+    link: "/card/02x54YnSJ7voLYWc0Bi0",
     iconUrl: "https://cdn4.iconfinder.com/data/icons/payment-method/160/payment_method_card_visa-512.png",
   },
 ];
