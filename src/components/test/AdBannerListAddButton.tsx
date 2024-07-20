976276dda0b70c1819407616a4c4bf6b8683a015
Suji Park <psjhero94@naver.com>
2024-07-20 22:53:25 +0900

    TEST 페이지 기능 차단

--- src/components/test/AdBannerListAddButton.tsx
+++ src/components/test/AdBannerListAddButton.tsx
@@ -7,17 +7,13 @@ import { COLLECTIONS } from "@constants";
 
 function AdBannerListAddButton() {
   const handleButtonClick = async () => {
-    const batch = writeBatch(store);
-
-    adBanners.forEach((banner) => {
-      const docRef = doc(collection(store, COLLECTIONS.ADBANNER));
-
-      batch.set(docRef, banner);
-    });
-
-    await batch.commit();
-
-    alert("배너 리스트 추가완료!");
+    // const batch = writeBatch(store);
+    // adBanners.forEach((banner) => {
+    //   const docRef = doc(collection(store, COLLECTIONS.ADBANNER));
+    //   batch.set(docRef, banner);
+    // });
+    // await batch.commit();
+    // alert("배너 리스트 추가완료!");
   };
 
   return <Button onClick={handleButtonClick}>배너 리스트 추가하기</Button>;
--- src/components/test/CardListAddButton.tsx
+++ src/components/test/CardListAddButton.tsx
@@ -7,17 +7,13 @@ import { COLLECTIONS } from "@constants";
 
 function CardListAddButton() {
   const handleButtonClick = async () => {
-    const batch = writeBatch(store);
-
-    card_list.forEach((card) => {
-      const docRef = doc(collection(store, COLLECTIONS.CARD));
-
-      batch.set(docRef, card);
-    });
-
-    await batch.commit();
-
-    alert("카드 리스트 추가완료!");
+    // const batch = writeBatch(store);
+    // card_list.forEach((card) => {
+    //   const docRef = doc(collection(store, COLLECTIONS.CARD));
+    //   batch.set(docRef, card);
+    // });
+    // await batch.commit();
+    // alert("카드 리스트 추가완료!");
   };
 
   return <Button onClick={handleButtonClick}>카드 리스트 추가하기</Button>;
